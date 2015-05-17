59f8482ef4308a1f4bd009f53d927476e06a8a4e
NikV <techvoltz@gmail.com>
2015-05-17 18:05:23 -0500

    Widget

--- iheart-quotes.php
+++ iheart-quotes.php
@@ -26,26 +26,28 @@ Class iheart_Quotes_Plugin {
 	 * The main Quotes Function
 	 */
 	public function iheart_quotes_function() {
-		if (! $joke = get_transient('iheart_quotes') ) {
+		if (! $fun_quote = get_transient('iheart_quotes') ) {
 			// If there's no cached version, let's get a joke
 			$jsonurl     = "http://www.iheartquotes.com/api/v1/random?format=json";
 			$json        = wp_remote_get( $jsonurl );
+
 			if ( is_wp_error( $json ) ) {
 				return "Chuck Norris accidentally kicked the server, it will be up soon!";
 			}
+
 			else {
 				// If everything's okay, parse the body and json_decode it
 				$json_output = json_decode( wp_remote_retrieve_body( $json ));
-				$quote = $json_output->quote;
+				$fun_quote = $json_output->quote;
 
 				// Store the result in a transient, expires after 1 day
 				// Also store it as the last successful using update_option
 				if ( $json_output->type = "success" ) {
-					set_transient( 'iheart_quotes', $quote, 60 * 1 );
+					set_transient( 'iheart_quotes', $fun_quote, 60 * 1 );
 				}
 			}
 		}
-		echo esc_html( $quote );
+		echo esc_html( $fun_quote );
 	}
 
 
@@ -75,3 +77,40 @@ Class iheart_Quotes_Plugin {
 
 }
 new iheart_Quotes_Plugin();
+
+
+class iHeart_Quotes_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		parent::__construct(
+			'iiheart_quotes_widget', // Base ID
+			__( 'Quotes Widget', 'text_domain' ), // Name
+			array( 'description' => __( 'A widget that displays a new quote every few minutes', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+
+
+		$classb = new iheart_Quotes_Plugin();
+		$classb->iheart_quotes_function();
+
+		echo $args['after_widget'];
+
+	}
+
+}
+
+add_action( 'widgets_init', function(){
+	register_widget( 'iHeart_Quotes_Widget' );
+});
